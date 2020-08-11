6cab0f7e0b76375a989a5aa4a26875360085e231
Lesley Rivera <les.aguilar@live.com>
2020-08-11 15:44:02 -0700

    dbContext

--- ECommerce_Application/ECommerce_Application/Startup.cs
+++ ECommerce_Application/ECommerce_Application/Startup.cs
@@ -1,12 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Configuration;
+using ECommerce_Application.Data;
 using ECommerce_Application.Models.Interfaces;
 using ECommerce_Application.Models.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,12 +13,29 @@ namespace ECommerce_Application
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddTransient<ICereal, CerealRepository>();
+            services.AddTransient<IProduct, CerealRepository>();
+
+            //Resgistering our DbContext
+            services.AddDbContext<StoreDbContext>(options =>
+            {
+                //Install-Package Microsoft.EntityFrameWorkCore.SqlServer
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddTransient<IProduct, CerealRepository>(); 
+
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

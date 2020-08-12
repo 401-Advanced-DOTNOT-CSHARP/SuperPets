3faa155e94a4cf1b05fdb9969b2122cfb3d1e4a8
Bryant Davis <bryant.davis@hotmail.com>
2020-08-12 15:13:24 -0700

    completed US 3 Task 2

--- ECommerce_Application/ECommerce_Application/Data/StoreDbContext.cs
+++ ECommerce_Application/ECommerce_Application/Data/StoreDbContext.cs
@@ -13,7 +13,36 @@ namespace ECommerce_Application.Data
         {
 
         }
-        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            #region
+            modelBuilder.Entity<Product>().HasData(
+
+                new Product
+                {
+                    Id = 1,
+                    Name = "Rampage",
+                     Age = 2,
+                      Breed = "Staffordshire Terrier",
+                       Color = "Brindle",
+                        Description = "Absolutely is an attention hog. He will chill and hang out with you all day and love life. He enjoys the outdoors " +
+                        "from sunbathing in the backyard to going for long walks. Is very obedient and will let you clean his ears, brush "
+                },
+                new Games
+                {
+                    Id = 2,
+                    Name = "The World of NonCraft"
+                },
+                new Games
+                {
+                    Id = 3,
+                    Name = "City of Zeroes"
+                });
+        }
+
         public DbSet<Product> Products { get; set; }
     }
 }
--- ECommerce_Application/ECommerce_Application/Models/Product.cs
+++ ECommerce_Application/ECommerce_Application/Models/Product.cs
@@ -12,5 +12,12 @@ namespace ECommerce_Application.Models.Services
         public string SKU { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
+        public string Breed { get; set; }
+        public int Age { get; set; }
+        public string SuperPower { get; set; }
+        public string Color { get; set; }
+
+
+
     }
 }

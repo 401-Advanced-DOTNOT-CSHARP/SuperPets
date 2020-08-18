f62851e9c6f48a94774ed6b9ee75ebff4fece11d
Olamide Ogunde <oogunde0413@gmail.com>
2020-08-18 08:21:15 -0700

    migrations

---
+++ ECommerce_Application/ECommerce_Application/Migrations/20200818034123_NewMigrationSeedForStore.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ECommerce_Application.Migrations
+{
+    public partial class NewMigrationSeedForStore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

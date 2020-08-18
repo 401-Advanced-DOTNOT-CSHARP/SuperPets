276f04a76419815f8f15516e3683d0c95d69a3ee
Bryant Davis <bryant.davis@hotmail.com>
2020-08-17 18:55:19 -0700

    finished blob

---
+++ ECommerce_Application/ECommerce_Application/Models/Interfaces/IImage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce_Application.Models.Interfaces
+{
+    public interface IImage
+    {
+        public Task UploadImage(string fileName, byte[] image, string contentType);
+        public Task<CloudBlobContainer> GetContainer(string containerName);
+        public Task<CloudBlob> GetBlob(string imageName, string containerName);
+    }
+}

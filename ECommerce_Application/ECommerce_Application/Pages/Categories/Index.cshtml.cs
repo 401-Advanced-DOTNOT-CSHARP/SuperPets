9fd16e144beb27112b476660826829dd46040a65
Bryant Davis <bryant.davis@hotmail.com>
2020-08-24 18:57:21 -0700

    cart is fully functional

--- ECommerce_Application/ECommerce_Application/Pages/Categories/Index.cshtml.cs
+++ ECommerce_Application/ECommerce_Application/Pages/Categories/Index.cshtml.cs
@@ -7,25 +7,52 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ECommerce_Application.Data;
 using ECommerce_Application.Models;
+using ECommerce_Application.Models.Interfaces;
 
 namespace ECommerce_Application.Pages.Categories
 {
     public class IndexModel : PageModel
     {
-        private readonly ECommerce_Application.Data.StoreDbContext _context;
+        private readonly ICartItem _cartItem;
+        private readonly ICart _cart;
 
-        public IndexModel(ECommerce_Application.Data.StoreDbContext context)
+        public IndexModel(ICart cart, ICartItem cartItem)
         {
-            _context = context;
+            _cart = cart;
+            _cartItem = cartItem;
+        }
+        [BindProperty]
+        public Cart Cart { get;set; }
+        [BindProperty]
+        public CartItem CartItem { get; set; }
+        [BindProperty]
+        public int ProductId { get; set; }
+        [BindProperty]
+        public int CartId { get; set; }
+        [BindProperty]
+        public int Quantity { get; set; }
+
+
+        public async Task<IActionResult> OnGet()
+        {
+;           Cart = await _cart.GetCart(User.Identity.Name);
+
+            return Page();
         }
 
-        public IList<CartItem> CartItem { get;set; }
-
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnPostDelete()
         {
-            CartItem = await _context.CartItems
-                .Include(c => c.Cart)
-                .Include(c => c.Product).ToListAsync();
+            CartItem = await _cartItem.GetCartItem(ProductId, CartId);
+            await _cartItem.RemoveProductFromCart(CartItem.Product, CartItem.Cart);
+
+            return RedirectToPage("./Index");
+        }
+
+        public async Task<IActionResult> OnPostUpdate()
+        {
+            CartItem = await _cartItem.GetCartItem(ProductId, CartId);
+            await _cartItem.UpdateCartQuantityAndPrice(CartItem.Product, CartItem.Cart, Quantity);
+            return new RedirectToPageResult("/Categories/Index");
         }
     }
 }

db528b418c70e52d1db2d392ff1eed9fe200cdc9
Olamide Ogunde <oogunde0413@gmail.com>
2020-08-25 17:36:29 -0700

    changes accepted

--- ECommerce_Application/ECommerce_Application/Pages/Categories/Checkout.cshtml.cs
+++ ECommerce_Application/ECommerce_Application/Pages/Categories/Checkout.cshtml.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using ECommerce_Application.Models;
 using ECommerce_Application.Models.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using static ECommerce_Application.Pages.Account.RegisterModel;
 
 namespace ECommerce_Application.Pages.Categories
 {
@@ -13,20 +16,26 @@ namespace ECommerce_Application.Pages.Categories
     {
         private readonly IOrder _order;
         private readonly ICart _cart;
+        private readonly ICartItem _cartitem;
         private readonly IPayment _payment;
+        private IEmailSender _sender;
 
-        public CheckoutModel(IOrder order, ICart cart, IPayment payment)
-        {
-            _order = order;
-            _cart = cart;
-            _payment = payment;
-        }
         [BindProperty]
         public Order Order { get; set; }
         [BindProperty]
         public Cart Cart { get; set; }
 
 
+        [BindProperty]
+        public RegisterViewModel Registration { get; set; }
+
+        public CheckoutModel(IOrder order, ICart cart, IPayment payment, IEmailSender sender)
+        {
+            _order = order;
+            _cart = cart;
+            _payment = payment;
+            _sender = sender;
+        }
 
 
         public async Task<IActionResult> OnGet()
@@ -36,14 +45,22 @@ namespace ECommerce_Application.Pages.Categories
         }
         public async Task<IActionResult> OnPost()
         {
+
+            Cart = await _cart.GetCart(User.Identity.Name);
+
+            string subject = $"Receipt of purchase from SuperPets for {Order.FirstName}";
+            string htmlMessage = $"Your Order was purchased on {Order.Date}!</h1><br>Order Id: {Order.CartId}</li></ul></h1><br><ul><li>Cart: {Cart.CartItems}</li></ul><ul><li>Shipping Address: {Order.Address}<br> {Order.City}<br>{Order.State}</li></ul>";
             Order.Date = DateTime.Now;
             Order.Cart = Cart;
             await _order.CreateOrder(Order);
             var payment = await _payment.Run(Order.UserEmail);
+            await _sender.SendEmailAsync(Order.UserEmail, subject, htmlMessage);
+
             Cart.Date = DateTime.Now;
             Cart cart = new Cart()
             {
                 UserEmail = User.Identity.Name,
+                
                 Date = DateTime.Now
             };
             await _cart.CreateCart(cart);

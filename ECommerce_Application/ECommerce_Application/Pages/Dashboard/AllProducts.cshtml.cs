9016e5db94e820b470e11af79b2bbf5a047525ff
Bryant Davis <bryant.davis@hotmail.com>
2020-08-30 11:52:14 -0700

    finished the dashboard, fixed the upload picture to take in a word for a name vs a drop down and added all the images in the blob to the create product page

---
+++ ECommerce_Application/ECommerce_Application/Pages/Dashboard/AllProducts.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce_Application.Models;
+using ECommerce_Application.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ECommerce_Application.Pages.Dashboard
+{
+    public class AllProductsModel : PageModel
+    {
+        private readonly IProduct _product;
+
+        public AllProductsModel(IProduct product)
+        {
+            _product = product;
+        }
+        [BindProperty]
+        public List<Product> Products { get; set; }
+        public async Task<IActionResult> OnGet()
+        {
+            Products = await _product.GetProducts();
+            return Page();
+        }
+    }
+}
